6f9b770d6390fed382fa2309ab1f1dda59815735
Ricardo Nunes <ricardonunesdev@gmail.com>
2017-09-15 17:48:05 +0100

    Added 2 basic tests for getRegion method

--- test/test.js
+++ test/test.js
@@ -1,9 +1,23 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 const EC2 = require('../');
 
-EC2.init('us-west-1');
+describe('Testing AWS EC2 Wrapper', () => {
 
-let res = EC2.launchInstance();
+    describe('Testing EC2.getRegion()', () => {
 
-console.log(res);
+        it('should return error if EC2 connection hasn\'t been initialized', () => {
+            expect(() => { EC2.getRegion() }).to.throw(EC2.errors.NOT_INITIALIZED);
+        });
+
+        it('should return the correct region', () => {
+            EC2.init('us-west-1');
+            let res = EC2.getRegion();
+            expect(res).to.be.equal('us-west-1');
+        });
+
+    });
+
+});
