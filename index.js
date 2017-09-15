b69588370e7c48f3825d985813c84a3657a9dfa8
Ricardo Nunes <ricardonunesdev@gmail.com>
2017-09-15 17:47:40 +0100

    Added method that gets the current region Added common error list Added method that checks if EC2 connection has been initialized Added more JSDoc

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const AWS = require('aws-sdk');
 let EC2 = null;
 
 /**
- * Initialize EC2 connection and set AWS region
+ * Checks if the EC2 connection has been initialized.
+ */
+const checkInitialized = () => {
+    if (EC2 === null) {
+        throw new Error(errors.NOT_INITIALIZED);
+    }
+};
+
+/**
+ * Initializes the EC2 connection and sets the AWS region.
  * @param {string} region The AWS region you want to interact with (example: 'us-west-1').
  */
 const init = (region) => {
@@ -15,14 +24,24 @@ const init = (region) => {
 };
 
 /**
- * Launch a new EC2 instance
+ * Gets the currently selected AWS region.
  * @return {string} The name of the AWS region
  */
-const launchInstance = () => {
+const getRegion = () => {
+    checkInitialized();
     return EC2.config.region;
 }
 
+/**
+ * Common error list.
+ */
+const errors = {
+    NOT_INITIALIZED: 'EC2 not initialized. Please call EC2.init() with a valid region'
+};
+
 module.exports = {
     init: init,
-    launchInstance: launchInstance
+    getRegion: getRegion,
+
+    errors: errors
 };
