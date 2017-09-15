605d3f35cb44f20fb9154b05c86701166d2afdaf
Ricardo Nunes <ricardonunesdev@gmail.com>
2017-09-15 17:16:20 +0100

    Added init method and placeholder for launch instance

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 'use strict';
 
-module.exports = {};
+const Promise = require('bluebird');
+const AWS = require('aws-sdk');
+
+let EC2 = null;
+
+/**
+ * Initialize EC2 connection and set AWS region
+ * @param {string} region The AWS region you want to interact with (example: 'us-west-1').
+ */
+const init = (region) => {
+    AWS.config.region = region;
+    EC2 = new AWS.EC2();
+};
+
+/**
+ * Launch a new EC2 instance
+ * @return {string} The name of the AWS region
+ */
+const launchInstance = () => {
+    return EC2.config.region;
+}
+
+module.exports = {
+    init: init,
+    launchInstance: launchInstance
+};
