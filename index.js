bacb4e5900e7a19203650dfaf4bbd24fef94d7cc
Ricardo Nunes <ricardonunesdev@gmail.com>
2017-09-15 18:10:34 +0100

    Added validation for EC2 regions

--- index.js
+++ index.js
@@ -14,13 +14,21 @@ const checkInitialized = () => {
     }
 };
 
+const isValidRegion = (region) => {
+    return (validRegions.indexOf(region) !== -1);
+};
+
 /**
  * Initializes the EC2 connection and sets the AWS region.
  * @param {string} region The AWS region you want to interact with (example: 'us-west-1').
  */
 const init = (region) => {
-    AWS.config.region = region;
-    EC2 = new AWS.EC2();
+    if (isValidRegion(region)) {
+        AWS.config.region = region;
+        EC2 = new AWS.EC2();
+    } else {
+        throw new Error(errors.INVALID_REGION);
+    }
 };
 
 /**
@@ -33,15 +41,37 @@ const getRegion = () => {
 }
 
 /**
+ * Valid AWS regions for EC2.
+ */
+const validRegions = [
+    'ap-northeast-1',
+    'ap-northeast-2',
+    'ap-south-1',
+    'ap-southeast-1',
+    'ap-southeast-2',
+    'ca-central-1',
+    'eu-central-1',
+    'eu-west-1',
+    'eu-west-2',
+    'sa-east-1',
+    'us-east-1',
+    'us-east-2',
+    'us-west-1',
+    'us-west-2'
+];
+
+/**
  * Common error list.
  */
 const errors = {
-    NOT_INITIALIZED: 'EC2 not initialized. Please call EC2.init() with a valid region'
+    NOT_INITIALIZED: 'EC2 not initialized. Please call EC2.init() with a valid region',
+    INVALID_REGION: 'Region is invalid. Please check valid regions for EC2 here - http://docs.aws.amazon.com/general/latest/gr/rande.html#ec2_region'
 };
 
 module.exports = {
     init: init,
     getRegion: getRegion,
 
+    validRegions: validRegions,
     errors: errors
 };
