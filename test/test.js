2905bcf57cbdc3971b7acee10cac2c1356c84ad0
Ricardo Nunes <ricardonunesdev@gmail.com>
2017-09-18 10:29:14 +0100

    Added test for getInstancesByState method

--- test/test.js
+++ test/test.js
@@ -25,7 +25,6 @@ describe('AWS EC2 Wrapper', () => {
     describe('EC2.getAllInstances()', () => {
         it('should return an array of instances', (done) => {
             EC2.init('eu-west-1');
-
             EC2.getAllInstances()
                 .then((instances) => {
                     expect(instances).to.be.an('array');
@@ -43,7 +42,6 @@ describe('AWS EC2 Wrapper', () => {
 
         it('should return an empty object', (done) => {
             EC2.init('eu-west-1');
-
             EC2.getInstanceByIpAddress('1.2.3.4')
                 .then((instance) => {
                     expect(instance).to.be.an('object');
@@ -66,11 +64,10 @@ describe('AWS EC2 Wrapper', () => {
         });
     });
 
-    describe('EC2.getInstanceByInstanceId()', () => {
+    describe('EC2.getInstanceById()', () => {
         it('should return the instance', (done) => {
             EC2.init('eu-west-1');
-
-            EC2.getInstanceByInstanceId('')
+            EC2.getInstanceById('')
                 .then((instance) => {
                     expect(instance).to.be.an('object');
                     expect(instance).to.be.empty;
@@ -81,11 +78,27 @@ describe('AWS EC2 Wrapper', () => {
 
         it('should return the instance', (done) => {
             EC2.init('eu-west-1');
-
-            EC2.getInstanceByInstanceId(process.env.INSTANCE_ID)
+            EC2.getInstanceById(process.env.INSTANCE_ID)
                 .then((instance) => {
                     expect(instance).to.be.an('object');
                     expect(instance).to.not.be.empty;
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
+    describe('EC2.getInstancesByState()', () => {
+        it('should return error if state is invalid', () => {
+            EC2.init('eu-west-1');
+            expect(() => { EC2.getInstancesByState('abc'); }).to.throw(EC2.errors.INVALID_STATE);
+        });
+
+        it('should return an array of running instances', (done) => {
+            EC2.init('eu-west-1');
+            EC2.getInstancesByState('running')
+                .then((instances) => {
+                    expect(instances).to.be.an('array');
                     done();
                 })
                 .catch(done);
